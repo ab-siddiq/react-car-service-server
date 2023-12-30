8abb8e50a31f6e108c3c075fcf6407df62797973
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-31 03:58:12 +0600

    create booking api to store booking data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 console.log(process.env.DB_USER);
@@ -34,6 +34,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
     const serviceCollection = client.db("carService").collection("services");
+    const bookingCollection = client.db("carService").collection("bookings");
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -41,27 +42,32 @@ async function run() {
     });
     app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
-      console.log('get id',id)
+      console.log("get id", id);
       const query = { _id: new ObjectId(id) };
       const options = {
-        projection: {title:1, price:1, service_id:1}
-      }
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
       try {
         const result = await serviceCollection.findOne(query, options);
-        console.log('q:', query);
-        console.log('r:', result);
-        
+        console.log("q:", query);
+        console.log("r:", result);
+
         if (!result) {
-            return res.status(404).send("Service not found");
+          return res.status(404).send("Service not found");
         }
 
         res.send(result);
-    } catch (err) {
+      } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Server error");
-    }
+      }
     });
-    
+    app.post('/bookings',async(req,res)=>{
+      const booking = req.body
+      console.log(booking)
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
