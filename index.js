bfaae04687b4f9321e45ddfbc9b8a1cabe375753
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-31 02:27:08 +0600

    create service api get service by id

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
-console.log(process.env.DB_USER)
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+console.log(process.env.DB_USER);
 //middleware
-app.use(cors()) //cors origin 
-app.use(express.json()) //convert req.body to json
+app.use(cors()); //cors origin
+app.use(express.json()); //convert req.body to json
 
-// app.get('/',req,res=>{
-
-// })
-
+app.get("/", (req, res) => {
+  res.send("server is running!!");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gjnzlrq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,15 +30,45 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const serviceCollection = client.db("carService").collection("services");
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log('get id',id)
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: {title:1, price:1, service_id:1}
+      }
+      try {
+        const result = await serviceCollection.findOne(query, options);
+        console.log('q:', query);
+        console.log('r:', result);
+        
+        if (!result) {
+            return res.status(404).send("Service not found");
+        }
+
+        res.send(result);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Server error");
+    }
+    });
+    
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.listen(port,()=>{
-  console.log(`listering to port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listering to port ${port}`);
+});
