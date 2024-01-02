de92f009bd631fa3ac2a55395f24be4707f85c2e
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2024-01-02 15:33:45 +0600

    create jwt token verification

--- index.js
+++ index.js
@@ -25,7 +25,25 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+const verifyJWT = (req, res, next) => {
+  console.log("hit jwt");
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify( token , process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -44,8 +62,8 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      console.log(token)
-      res.send({token});
+      console.log(token);
+      res.send({ token });
     });
     //Services
     app.get("/services", async (req, res) => {
@@ -76,9 +94,13 @@ async function run() {
       }
     });
     //bookings
-    app.get("/bookings", async (req, res) => {
-      // console.log(req.query)
+    app.get("/bookings",verifyJWT, async (req, res) => {
+      // console.log(req.headers.authorization)
       let query = {};
+      const decoded = req.decoded
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: 1, message: 'forbiddedn access'})
+      }
       if (req.query?.email) {
         query = { email: req.query.email };
       }
